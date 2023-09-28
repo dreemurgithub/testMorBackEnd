80181fd0d46c355163a777def483854fc78e034f
LeThanhDat <lethanhdat1993@gmail.com>
2023-09-28 16:38:34 +0700

    update with env on docker-compose.yaml

--- dockerfile
+++ dockerfile
@@ -19,12 +19,7 @@ COPY . .
 # Expose the desired port (if your Node.js app listens on a specific port)
 EXPOSE 3000
 
-# Environment variables
-ENV POSTGRES_HOST=db
-ENV POSTGRES_PORT=5432
-ENV POSTGRES_USER=postgres
-ENV POSTGRES_PASSWORD=mysecretpassword
-ENV POSTGRES_DB=mydatabase
+# Environment variables, but should specify on docker-compose.yaml
 
 # Start the Node.js application - sudo docker run -p 3000:3000 CrudTodo
 CMD ["npm", "start"]
--- index.js
+++ index.js
@@ -1,36 +1,16 @@
 require("dotenv").config();
+const express = require('express')
 const app = express();
 const port = 3000;
-// import crypto from "crypto";
-// import fs from "fs";
-// import path from "path";
-const pathToRoutes = path.join(__dirname, "./routes/filePath");
-console.log(pathToRoutes);
-
-// import { user } from "./routes/pathURL";
-// import { userController } from "./controllers/user";
-// import { fileOne, fileTwo } from "./routes/filePath";
 app.use(express.json());
-app.use(userController);
 
 app.get("/", (req, res) => {
-  // import file from './routes/filePath'
-  fs.readFile(fileOne, "utf-8", (err, data) => {
-    
-  });
-  // res.send(text);
+  res.send('hello world')
 });
 
-// app.post("/", (req, res) => {
-//   // import file from './routes/filePath'
-//   fs.readFile(fileTwo, "utf-8", (err, data) => {
-    
-//   });
-//   // res.send(text);
-// });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port} - build1`);
   // console.log(process.env);
-  console.log(process.env.DOCKER_DATABASE_URL);
+  console.log(process.env);
 });
