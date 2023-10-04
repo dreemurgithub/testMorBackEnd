f005a528e809606a054f168bf3180f622ecfb825
LeThanhDat <lethanhdat1993@gmail.com>
2023-10-04 17:33:52 +0700

    create todo session

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(cookieParser());
 app.use(cors({}));
 app.use(express.json());
 
-const authMiddleware = require('./controller/authMiddleware')
-const authRoute = require('./controller/auth/index')
+const authMiddleware = require("./controller/authMiddleware");
+const authRoute = require("./controller/auth/index");
 const { pool } = require("./models/postgresJs/index");
 const connectRoute = require("./controller/connect/index");
 const commentRoute = require("./controller/comment/index");
@@ -29,37 +29,56 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
-      secure:  process.env.ENVIROMENT === "DEV" ? "auto" :true,
-      sameSite: process.env.ENVIROMENT === "DEV"? "lax" : "none",
+      secure: process.env.ENVIROMENT === "DEV" ? "auto" : true,
+      sameSite: process.env.ENVIROMENT === "DEV" ? "lax" : "none",
     }, // 30 days
     // for browser security requirement on https
   })
 );
-app.use(authMiddleware)
-app.use(authRoute)
+app.use(authMiddleware);
+app.use(authRoute);
 app.use(connectRoute);
 app.use(commentRoute);
 app.use(todoRoute);
 app.use(usertRoute);
 app.listen(process.env.Port, async () => {
   const client = await pool.connect();
-  const queryUser = `SELECT EXISTS (
+  // user exist?
+  const queryUserExist = `SELECT EXISTS (
   SELECT 1
   FROM pg_tables
   WHERE schemaname = 'public'
   AND tablename = 'users'
   );`;
 
-  const userExist = await pool.query(queryUser);
+  const userExist = await pool.query(queryUserExist);
 
   const createTableQuery = `CREATE TABLE users (id SERIAL PRIMARY KEY,username VARCHAR(50), email VARCHAR(100),password VARCHAR(100),created_at TIMESTAMP DEFAULT NOW(), updated_at TIMESTAMP);`;
   if (!userExist.rows[0].exists) await pool.query(createTableQuery);
-
-  const querySession = `SELECT EXISTS (
+// todo exist?
+  const queryTodoExist = `SELECT EXISTS (
     SELECT 1
     FROM pg_tables
-    WHERE tablename = 'user_sessions'
+    WHERE schemaname = 'public'
+    AND tablename = 'todo'
     );`;
+  const todoExist = await pool.query(queryTodoExist);
+
+  const createTodoQuery = `CREATE TABLE todo (
+    todo_id SERIAL PRIMARY KEY,
+    task TEXT NOT NULL,
+    status TEXT,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT current_timestamp
+    );`;
+  if (!todoExist.rows[0].exists) await pool.query(createTodoQuery);
+
+  // users_session exists?
+  const querySession = `SELECT EXISTS (
+        SELECT 1
+        FROM pg_tables
+        WHERE tablename = 'user_sessions'
+        );`;
 
   const sessionExist = await pool.query(querySession);
 
@@ -75,30 +94,4 @@ app.listen(process.env.Port, async () => {
   client.release();
 
   console.log(`Server is running on port ${process.env.Port} - build1`);
-});
-
-app.get("/", async (req, res) => {
-  req.session.user = true;
-  console.log(req.session.data);
-  res.send(req.session);
-});
-
-app.get("/a", async (req, res) => {
-  res.send(req.session);
-});
-
-app.post("/", async (req, res) => {
-  req.session.user = true;
-  console.log(req.session.data);
-  res.send(req.session);
-});
-
-app.put("/", async (req, res) => {
-  res.send(req.session);
-});
-
-app.delete("/", async (req, res) => {
-  console.log(req.session);
-  req.session.destroy();
-  res.send("session here");
 });
